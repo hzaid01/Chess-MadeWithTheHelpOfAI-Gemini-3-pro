1ac1bb6e3eabcf47e19bb3d15bc5f507c6a3d7ee
Your Name <you@example.com>
2025-12-11 12:35:53 +0500

    commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zen.chess',
+  appName: 'Zen Chess',
+  webDir: 'dist'
+};
+
+export default config;
